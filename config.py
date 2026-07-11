9dfc92ef1a1877d660a3caefc7438ae6d4dbd799
karan243coder <152502742+karan243coder@users.noreply.github.com>
2026-07-11 10:13:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,4 +15,4 @@ PORT = 8080
 # Pyrogram MTProto 2GB Direct Stream Engine (Get for free from my.telegram.org)
 API_ID = 0          # Jaise: 1234567
 API_HASH = ""       # Jaise: "0123456789abcdef0123456789abcdef"
-SERVER_URL = ""     # Jaise: "https://familiar-gertrudis-botakingtipd-f3991937.koyeb.app"
+SERVER_URL = "https://theoretical-kynthia-mychool-a6f2b3d0.koyeb.app"
